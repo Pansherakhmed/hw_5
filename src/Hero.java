74298f062ed35f403518db5bed1ed8a197f6d521
Pansherakhmed <aliakhmedd@icloud.com>
2023-04-16 00:00:04 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,33 @@
+public class Hero {
+    private int health;
+    private int damage;
+    private int superPower;
+
+    public Hero(){
+
+    }
+    public Hero(int health, int damage, int superPower){
+        this.health = health;
+        this.damage = damage;
+        this.superPower = superPower;
+    }
+
+    public Hero(int health, int damage){
+        this.health = health;
+        this.damage = damage;
+
+    }
+    public int getHealth(){
+        return health;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public int getSuperPower(){
+        return superPower;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Boss boss1 = new Boss();
+        boss1.setHealth(500);
+        boss1.setDamage(50);
+        boss1.setDefence(500);
+        System.out.println(boss1.getHealth() + " " + boss1.getDamage() + " " + boss1.getDamage());
+
+
+    }
+}
